d8bd3d7d5705203e49c924e65bdecc3510dc6ed1
Stingo <brinis.nadjib.lp@gmail.com>
2023-01-03 23:09:50 +0100

    Added Signin Route and handler

---
+++ api/controllers/auth/signin.js
@@ -0,0 +1,18 @@
+const Member = require("../../../db/models/member");
+const hash = (password) =>
+  require("crypto").createHash("sha256").update(password).digest("hex");
+const Signin = async (req, res) => {
+  let data = req.body;
+  try {
+    let user = await Member.findOne({
+      email: data.email,
+      hashedPassword: hash(data.password),
+    });
+    res.send(user? {user} : {error:"Invalid credentials"} );
+  } catch (error) {
+    res.send({error});
+  }
+};
+
+module.exports = Signin;
+
--- api/routes/router.js
+++ api/routes/router.js
@@ -2,9 +2,10 @@ const express = require("express")
 const router = express.Router()
 const checkBody = require("../middlewares/checkBody")
 const Signup = require("../controllers/auth/signup")
+const Signin = require("../controllers/auth/signin")
 
 
-router.route("/allo").post(checkBody,Signup)
-
+router.route("/signup").post(checkBody,Signup)
+router.route("/signin").post(checkBody,Signin)
 
 module.exports = router
